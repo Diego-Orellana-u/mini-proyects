74e799ac9fa648ff08df16eff77dc91ee432197a
Diego Orellana <diego.orellana.git@gmail.com>
2024-01-21 15:42:32 -0300

    created two new pages for the react router

--- react-projects/movie-searcher/src/App.jsx
+++ react-projects/movie-searcher/src/App.jsx
@@ -87,3 +87,19 @@ export function App () {
         
     )
 }
+
+let ladder = {
+    step: 0,
+    up() {
+        this.step++;
+        return ladder
+    },
+    down() {
+        this.step--;
+        return ladder
+    },
+    showStep: function() { // shows the current step
+      alert( this.step );
+      return ladder
+    }
+};
