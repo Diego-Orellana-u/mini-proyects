45a23966f5dc1199429e35c693ea1be32c8a4dcb
Diego Orellana <diego.orellana.git@gmail.com>
2024-01-23 13:42:33 -0300

    changed router to declarative

--- react-projects/react-router/src/Router.jsx
+++ react-projects/react-router/src/Router.jsx
@@ -1,12 +1,10 @@
-import { useEffect } from "react"
-import { useState } from "react"
+import { useState, useEffect, Children } from "react"
 import { EVENTS } from "./consts"
 import { match } from "path-to-regexp"
 
 
 export function Router ({ children , routes = [], defaultComponent: DefaultComponent = () =>
 <h1>404</h1> }) {
-    console.log(children)
     const [ currentPath, setCurrentPath ] = useState(window.location.pathname)
   
     useEffect(() => {
@@ -24,6 +22,16 @@ export function Router ({ children , routes = [], defaultComponent: DefaultCompo
     },[])
 
     let routeParams = {}
+
+    const routesFromChildren = Children.map(children, ({ props, type }) => {
+      const { name } = type
+      const isRoute = name === 'Route'
+      if(!isRoute) return null
+
+      return props
+    })  
+
+    console.log(routesFromChildren)
   
     const Page = routes.find(({ path }) => {
       if (path === currentPath) return true 
