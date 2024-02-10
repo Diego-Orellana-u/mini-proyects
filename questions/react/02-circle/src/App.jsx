b976ae1a73d7822fc2bd4fa107bfc85ab521cce1
Diego Orellana <diego.orellana.git@gmail.com>
2024-02-09 23:55:21 -0300

    finished prototype of circle creator in react

--- questions/react/02-circle/src/App.jsx
+++ questions/react/02-circle/src/App.jsx
@@ -3,25 +3,56 @@ import './App.css'
 
 export default function App() {
   const [ mousePosition, setMousePosition ] = useState({x: undefined, y: undefined})
+  const [ lastCircle, setLastCircle ] = useState([])
 
   const handleClick = (e) => {
     setMousePosition(prevState => ({...prevState, x: e.clientX, y: e.clientY}))
   }
 
   const createCircle = () => {
-    let circle = <span className='circle' style={{left: mousePosition.x, top: mousePosition.y}}></span>
-    document.body.appendChild(circle)
+    let container = document.querySelector(".page")
+    let circle = document.createElement("div")
+    circle.classList.add('circle')
+    container.appendChild(circle)
+    circle.style.left = `${mousePosition.x - 17}px`
+    circle.style.top = `${mousePosition.y - 60}px`
   }
 
   useEffect(() => {
+    if(!mousePosition.x) return
     createCircle()
   },[mousePosition])
+
+  const handleUndo = () => {
+    let page = document.querySelector('.page')
+    let lastChild = page.lastChild
+    let lastToStr = lastChild.outerHTML
+
+    let lastCircleCopy = [...lastCircle, lastToStr ]
+    setLastCircle(lastCircleCopy)
+    lastChild.remove()
+  }
+  console.log(lastCircle)
+
+  const handleRedo = () => {
+    let page = document.querySelector('.page')
+    let redoChild = lastCircle[lastCircle.length - 1]
+
+    let childNode = new DOMParser().parseFromString(redoChild, 'text/html').body.lastChild
+    page.appendChild(childNode)
+  }
   
 
-  console.log(mousePosition)
-
   return (
-    <div className='page' onClick={handleClick}>
-    </div>
+    <>
+      <button className='button' onClick={handleUndo}>
+        UNDO
+      </button>
+      <button className='button' onClick={handleRedo}>
+        REDO
+      </button>
+      <div className='page' onClick={handleClick}>
+      </div>
+    </>
   )
 }
