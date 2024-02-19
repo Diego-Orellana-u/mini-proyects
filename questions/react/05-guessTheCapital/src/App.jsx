6fd85bb4c7d4addac29ed5882d6e89d99743895c
Diego Orellana <diego.orellana.git@gmail.com>
2024-02-19 13:32:22 -0300

    fixed a few bugs in guess the capital

--- questions/react/05-guessTheCapital/src/App.jsx
+++ questions/react/05-guessTheCapital/src/App.jsx
@@ -20,6 +20,7 @@ export default function App(){
     setOrder(order)
 
   }
+  
   function randomizer(array){
     for(let i = array.length - 1; i > 0;i--){
       const j = Math.floor(Math.random() * (i + 1));
