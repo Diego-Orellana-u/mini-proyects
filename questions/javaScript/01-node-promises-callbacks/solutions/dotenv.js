f872684f85ec5e0e97771d88b03900f6ac099f94
Diego Orellana <diego.orellana.git@gmail.com>
2024-03-25 16:33:16 -0300

    created my own dotenv for a technical interview

--- questions/javaScript/01-node-promises-callbacks/solutions/dotenv.js
+++ questions/javaScript/01-node-promises-callbacks/solutions/dotenv.js
@@ -1,19 +1,22 @@
 import fs from "node:fs";
 
-const dotenv = {
-  process: {
-    env: {},
-  },
-
-  config: function (path = ".env") {
+export function config({ path = ".env" } = {}) {
+  try {
     const params = fs.readFileSync(path, "utf-8");
 
     const chumi = params.replace(/"/g, "").split("\n");
-    const mingus = chumi.map((item) => item.split("="));
-    for (let item of mingus) {
-      process.env[item[0]] = item[1];
-    }
-  },
+    const mingus = chumi.map((item) => {
+      const [key, ...value] = item.split("=");
+      const joinedValue = value.join("=");
+      process.env[key] = joinedValue;
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const dotenv = {
+  config,
 };
 
 export default dotenv;
