ca03f061b09d6cbbfa4ae03e15a3546a478fe828
diego orellana <diego.orellana.git@gmail.com>
2024-10-18 18:11:25 -0300

    started 06 part of typescript course

--- typescript-courses/packages/notes-ts-fundamentals-v4/src/06-union-and-intersection-types.ts
+++ typescript-courses/packages/notes-ts-fundamentals-v4/src/06-union-and-intersection-types.ts
@@ -14,16 +14,14 @@ type Evens = 2 | 4 | 6 | 8
 let evenNumber: Evens = 2 //✔️ Valid
 // evenNumber = 5; //! 5 is not in the set
 
-/*
 // //? A set of numbers from 1 to 5 OR a set of even numbers from 1 to 9
-// let evenOrLowNumber = 5 as Evens | OneThroughFive;
+let evenOrLowNumber = 5 as Evens | OneThroughFive
 
-/*
 // //? Control flow sometimes results in union types
-// function flipCoin() {
-//     if (Math.random() > 0.5) return "heads"
-//     return "tails"
-// }
+function flipCoin() {
+  if (Math.random() > 0.5) return 'heads'
+  return 'tails'
+}
 
 // const outcome = flipCoin()
 // //     ^? "heads" | "tails"
