91de3b8a86a5d33107bfa568a60aec8d0daf21ea
diego orellana <diego.orellana.git@gmail.com>
2024-12-08 12:33:54 -0300

    updated app

--- pythonenv/storefront/playground/urls.py
+++ pythonenv/storefront/playground/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path 
-from . import views # the dot means that the file is in the current folder
+from django.urls import path
+from . import views
 
+# URLConf
 urlpatterns = [
-  path('hello/', views.say_hello) # we only pass the function reference
+    path('hello/', views.say_hello)
 ]
--- pythonenv/storefront/playground/views.py
+++ pythonenv/storefront/playground/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from store.models import Product
+from store.models import Customer
+from store.models import Collection
+from store.models import Order
+from store.models import OrderItem
+from django.db.models import Q, F
+from django.core.exceptions import ObjectDoesNotExist
 
-# Create your views here.
 def say_hello(request):
-  return render(request, 'hello.html', {'name': "Diego"})
+    # OrderItems = OrderItem.objects.values('product__title').distinct().order_by('product__title')
+
+    # queryset = Product.objects.filter(id__in=OrderItem.objects.values('product_id')).distinct().order_by('title')
+    
+    # OrderItems = Product.objects.filter(orderitem__isnull=False).distinct().order_by('title')
+
+    OrderItems = Product.objects.only('id', 'title', 'unit_price')
+
+
+    return render(request, 'hello.html', {'name': 'Mosh', 'products': OrderItems,  })
+
+
+# customers = Customer.objects.filter(email__contains=".com")
+# collections = Collection.objects.filter(featured_product_id__isnull=True)
+# products_lowInventory = Product.objects.filter(inventory__lt=10)
+# orderIdOne = Order.objects.filter(customer_id = 1)
+# productsCollectionThree = Product.objects.filter(collection_id = 3).order_by("last_update")
+# customers, "collections": collections, "lowInventory": products_lowInventory, "orderIdOne": orderIdOne, "productsCollectionThree": productsCollectionThree
+
+
+
+    # OrderItems = Product.objects.filter(orderitem__product_id=F('id')).order_by('title')
