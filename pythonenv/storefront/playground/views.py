3117eadb67dd3985718b6e7eab08738230fbc417
diego orellana <diego.orellana.git@gmail.com>
2024-12-09 11:53:32 -0300

    modified views in playground

--- pythonenv/storefront/playground/views.py
+++ pythonenv/storefront/playground/views.py
@@ -6,27 +6,17 @@ from store.models import Order
 from store.models import OrderItem
 from django.db.models import Q, F
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models.aggregates import Count, Max, Min, Avg, Sum
+
+from django.db.models.functions import Concat
+
+from django.db.models import Value, Func, ExpressionWrapper, DecimalField
 
 def say_hello(request):
-    # OrderItems = OrderItem.objects.values('product__title').distinct().order_by('product__title')
 
-    # queryset = Product.objects.filter(id__in=OrderItem.objects.values('product_id')).distinct().order_by('title')
-    
-    # OrderItems = Product.objects.filter(orderitem__isnull=False).distinct().order_by('title')
+    discounted_price = ExpressionWrapper(F('unit_price') * 0.8, output_field=DecimalField())
 
-    OrderItems = Product.objects.only('id', 'title', 'unit_price')
+    annotate = Product.objects.annotate(discounted_price=discounted_price)
 
 
-    return render(request, 'hello.html', {'name': 'Mosh', 'products': OrderItems,  })
-
-
-# customers = Customer.objects.filter(email__contains=".com")
-# collections = Collection.objects.filter(featured_product_id__isnull=True)
-# products_lowInventory = Product.objects.filter(inventory__lt=10)
-# orderIdOne = Order.objects.filter(customer_id = 1)
-# productsCollectionThree = Product.objects.filter(collection_id = 3).order_by("last_update")
-# customers, "collections": collections, "lowInventory": products_lowInventory, "orderIdOne": orderIdOne, "productsCollectionThree": productsCollectionThree
-
-
-
-    # OrderItems = Product.objects.filter(orderitem__product_id=F('id')).order_by('title')
+    return render(request, 'hello.html', {'name': 'Mosh', 'result': list(annotate), })
