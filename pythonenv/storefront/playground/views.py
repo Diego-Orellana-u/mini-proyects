5a4f0c44261864f1112c9c7ed07df4da9abca8a0
diego orellana <diego.orellana.git@gmail.com>
2024-12-15 17:56:23 -0300

    modified views

--- pythonenv/storefront/playground/views.py
+++ pythonenv/storefront/playground/views.py
@@ -3,11 +3,13 @@ from store.models import Product, Collection, Order, OrderItem
 from tags.models import TaggedItem
 from django.db import transaction
 
+from django.db import connection
+
 from django.contrib.contenttypes.models import ContentType
 
 
 def say_hello(request):
-
-    Product.objects.raw('SELECT * FROM store_product')
+    collection = Collection()
+    collection.objects.filter(pk__gt=10).delete()
 
     return render(request, 'hello.html', {'name': 'Mosh', })
