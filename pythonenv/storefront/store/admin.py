1bc7bdcbcf208f45bbcdf0aed374369bea0cc105
diego orellana <diego.orellana.git@gmail.com>
2024-12-18 15:52:22 -0300

    modified store admin

--- pythonenv/storefront/store/admin.py
+++ pythonenv/storefront/store/admin.py
@@ -1,4 +1,54 @@
 from django.contrib import admin
 from . import models
+from django.db.models import Count
 # Register your models here.
-admin.site.register(models.Collection)
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+  list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
+  list_editable = ['unit_price']
+  list_per_page = 10
+  list_select_related = ['collection']
+
+  def collection_title(self, product):
+    return product.collection.title
+
+  @admin.display(ordering='inventory')
+  def inventory_status(self, product):
+    if product.inventory < 10:
+      return 'Low'
+    return 'Ok'
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+  list_display = ['first_name', 'last_name', 'membership']
+  list_editable = ['membership']
+  ordering = ['first_name', 'last_name']
+  list_per_page = 10
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+  list_display = ['id', 'placed_at', 'payment_status', 'customer_name']
+  ordering = ['placed_at']
+  list_per_page = 10
+
+  @admin.display(ordering='customer__first_name')
+  def customer_name(self, order):
+    return f'{order.customer.first_name} {order.customer.last_name}' 
+  
+
+
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+  list_display = ['title', 'products_count']
+
+  @admin.display(ordering='products_count')
+  def products_count(self, collection):
+    return collection.products_count
+  
+
+  def get_queryset(self, request):
+    return super().get_queryset(request).annotate(
+      products_count=Count('products')
+    )
