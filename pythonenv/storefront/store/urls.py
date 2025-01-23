4e32b1af80f41cc4e4db07444a25fd9f93ca4d1b
diego orellana <diego.orellana.git@gmail.com>
2025-01-23 14:56:33 -0300

    modified urls

--- pythonenv/storefront/store/urls.py
+++ pythonenv/storefront/store/urls.py
@@ -4,7 +4,7 @@ from . import views
 # URLConf
 urlpatterns = [
     path('products/', views.product_list),
-    path('products/<int:id>/', views.product_detail),
-    path('collections/<int:id>/', views.collection_detail) 
+    path('products/<int:pk>/', views.product_detail),
+    path('collections/<int:pk>/', views.collection_detail, name="collection-detail") 
 ]
 
