0e2b063aa44f70883c8fbd1b4d2a55c77a25753d
diego orellana <diego.orellana.git@gmail.com>
2025-01-24 16:43:38 -0300

    added deserializers in views

--- pythonenv/storefront/store/views.py
+++ pythonenv/storefront/store/views.py
@@ -3,18 +3,23 @@ from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product, Collection
+from .serializers import ProductSerializer, CollectionSerializer
 
 @api_view()
 def collection_detail(request, pk):
-   return Response('ok')
+   collection = get_object_or_404(Product, pk=pk)
+   serializer = CollectionSerializer(collection)
+   return Response(serializer.data)
 
-@api_view()
+@api_view(['GET', 'POST'])
 def product_list(request):
-  queryset = Product.objects.select_related('collection').all()
-  serializer = ProductSerializer(queryset, many=True)
-  return Response(serializer.data)
+  if request.method == 'GET':
+    queryset = Product.objects.select_related('collection').all()
+    serializer = ProductSerializer(queryset, many=True, context={'request': request})
+    return Response(serializer.data)
+  
+  elif request.method == 'POST'
 
 
 @api_view()
