4c60f6f8e7785f95b825ed5348f9f77556d94bd2
diego orellana <diego.orellana.git@gmail.com>
2025-01-25 15:03:11 -0300

    modified serializers

--- pythonenv/storefront/store/serializers.py
+++ pythonenv/storefront/store/serializers.py
@@ -7,15 +7,18 @@ from .models import Collection
 class CollectionSerializer(serializers.ModelSerializer):
   class Meta: 
     model = Collection
-    fields = ['id', 'title']
+    fields = ['id', 'title', 'products_count']
+
+  products_count = serializers.IntegerField()
 
 class ProductSerializer(serializers.ModelSerializer):
   class Meta:
     model = Product
-    fields = ['id', 'title', 'unit_price', 'collection', 'price_with_tax']
+    fields = ['id', 'title', 'unit_price', 'inventory', 'description', 'slug', 'collection', 'price_with_tax']
 
   price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
 
   def calculate_tax(self, product):
     return product.unit_price * Decimal(1.1)
+  
   
