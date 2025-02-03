3578cf6d618a70cd87e94b799678725701691867
diego orellana <diego.orellana.git@gmail.com>
2025-02-03 18:13:12 -0300

    modified url comments

--- pythonenv/storefront/store/urls.py
+++ pythonenv/storefront/store/urls.py
@@ -4,7 +4,8 @@ from . import views
 # URLConf
 urlpatterns = [
     path('products/', views.product_list),
-    path('products/<int:pk>/', views.product_detail),
+    # Added type validations and dynamic data
+    path('products/<int:pk>/', views.product_detail), 
     path('collections/', views.collection_list),
     path('collections/<int:pk>/', views.collection_detail, name="collection-detail") 
 ]
