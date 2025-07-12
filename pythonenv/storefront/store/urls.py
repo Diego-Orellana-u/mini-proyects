4045b2e0e294bf060a46a1a186156e6706785fdc
diego orellana <diego.orellana.git@gmail.com>
2025-07-12 06:43:22 -0400

    created new url routers

--- pythonenv/storefront/store/urls.py
+++ pythonenv/storefront/store/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
+from django.urls import include
+
+router = DefaultRouter()
+router.register('cart', views.CartViewSet)
+router.register('cartitem', views.CartItemViewSet)
 
 # URLConf
 urlpatterns = [
+    path('', include(router.urls)),
     path('products/', views.product_list),
-    # Added type validations and dynamic data
     path('products/<int:pk>/', views.product_detail), 
     path('collections/', views.collection_list),
     path('collections/<int:pk>/', views.collection_detail, name="collection-detail") 
