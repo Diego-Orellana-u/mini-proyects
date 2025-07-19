5c29acff90e3dacfd9cbcd7e9cbddd5013113167
diego orellana <diego.orellana.git@gmail.com>
2025-07-19 13:45:11 -0400

    modified models str

--- pythonenv/storefront/store/models.py
+++ pythonenv/storefront/store/models.py
@@ -92,9 +92,12 @@ class Cart(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.title
+
 
 class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='item')
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField()
 
