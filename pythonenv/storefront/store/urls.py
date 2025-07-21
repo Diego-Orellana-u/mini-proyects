8276ef561bef9b4cf1d999071b5571c31a845878
diego orellana <diego.orellana.git@gmail.com>
2025-07-21 09:38:07 -0400

    added nested router to urls.py

--- pythonenv/storefront/store/urls.py
+++ pythonenv/storefront/store/urls.py
@@ -2,14 +2,19 @@ from django.urls import path
 from . import views
 from rest_framework.routers import DefaultRouter
 from django.urls import include
+from rest_framework_nested import routers
 
 router = DefaultRouter()
 router.register('cart', views.CartViewSet)
-router.register('cartitem', views.CartItemViewSet)
+
+cartitems_router = routers.NestedDefaultRouter(router, 'cart', lookup='cart')
+cartitems_router.register('items', views.CartItemViewSet, basename='cart-items')
+
 
 # URLConf
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(cartitems_router.urls)),
     path('products/', views.product_list),
     path('products/<int:pk>/', views.product_detail), 
     path('collections/', views.collection_list),
