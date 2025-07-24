00330bd6c1c0278599bc9497d0a7bfb0b774a030
diego orellana <diego.orellana.git@gmail.com>
2025-07-24 06:32:11 -0400

    modified serializers

--- pythonenv/storefront/store/serializers.py
+++ pythonenv/storefront/store/serializers.py
@@ -2,8 +2,6 @@ from decimal import Decimal
 from rest_framework import serializers
 from .models import Product, Cart, CartItem
 from .models import Collection
-from django.db.models import  F
-
 class CollectionSerializer(serializers.ModelSerializer):
   class Meta: 
     model = Collection
@@ -22,40 +20,37 @@ class ProductSerializer(serializers.ModelSerializer):
   def calculate_tax(self, product):
     return product.unit_price * Decimal(1.1)
   
-
 class SimpleProductSerializer(serializers.ModelSerializer):
   class Meta:
-      model = Product
-      fields = ['id','title', 'unit_price']
-
+    model = Product
+    fields = ['id', 'title', 'unit_price']
 
 class CartItemSerializer(serializers.ModelSerializer):
+  product = SimpleProductSerializer()
   total_price = serializers.SerializerMethodField(method_name='get_total_price')
-  product = SimpleProductSerializer(read_only=True)
 
-  product_id = serializers.PrimaryKeyRelatedField(
-      source='product',
-      queryset=Product.objects.all(),
-      write_only=True
-  )
+  def get_total_price(self, product):
+    return product.quantity * product.product.unit_price
 
-
-  def get_total_price(self, cartItem):
-    return cartItem.product.unit_price * cartItem.quantity
-  
   class Meta:
     model = CartItem
-    fields = ['id', 'product', 'product_id', 'quantity', 'total_price']
-  
+    fields = ['id', 'quantity', 'product', 'total_price']
 
 class CartSerializer(serializers.ModelSerializer):
-  id = serializers.UUIDField(read_only=True)
-  items = CartItemSerializer(read_only=True, many=True)
+  items = CartItemSerializer(many=True)
   total_price = serializers.SerializerMethodField(method_name='get_total_price')
 
+  # Anotar esto que no pude hacerlo
   def get_total_price(self, cart):
     return sum([item.quantity * item.product.unit_price for item in cart.items.all()])
   
   class Meta:
     model = Cart
     fields = ['id', 'items', 'total_price']
+
+class AddCartItemSerializer(serializers.ModelSerializer):
+  product_id = serializers.IntegerField()
+
+  class Meta:
+    model = CartItem
+    fields = ['id', 'product_id', 'quantity']
