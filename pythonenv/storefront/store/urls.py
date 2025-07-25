66c70c20d4751222f9ed4ddbd1a0ba15107ce7a4
diego orellana <diego.orellana.git@gmail.com>
2025-07-25 07:00:45 -0400

    modified urls to add nested route

--- pythonenv/storefront/store/urls.py
+++ pythonenv/storefront/store/urls.py
@@ -7,14 +7,14 @@ from rest_framework_nested import routers
 router = DefaultRouter()
 router.register('cart', views.CartViewSet)
 
-cartitems_router = routers.NestedDefaultRouter(router, 'cart', lookup='cart')
-cartitems_router.register('items', views.CartItemViewSet, basename='cart-items')
-
+# anotar esto que no pude hacerlo
+carts_router = routers.NestedDefaultRouter(router, 'cart', lookup='cart')
+carts_router.register('items', views.CartItemViewSet, basename='cart-items')
 
 # URLConf
 urlpatterns = [
     path('', include(router.urls)),
-    path('', include(cartitems_router.urls)),
+    path('', include(carts_router.urls)),
     path('products/', views.product_list),
     path('products/<int:pk>/', views.product_detail), 
     path('collections/', views.collection_list),
