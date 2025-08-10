0beb8630d3c89967aa2f8cb31e5133b039cca995
diego orellana <diego.orellana.git@gmail.com>
2025-08-10 06:43:09 -0400

    added new serializers to core

--- pythonenv/storefront/core/serializers.py
+++ pythonenv/storefront/core/serializers.py
@@ -1,4 +1,4 @@
-from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
+from djoser.serializers import UserSerializer as BaseUserSerializer, UserCreateSerializer as BaseUserCreateSerializer
 from rest_framework import serializers
 
 class UserCreateSerializer(BaseUserCreateSerializer):
@@ -6,3 +6,7 @@ class UserCreateSerializer(BaseUserCreateSerializer):
   
   class Meta(BaseUserCreateSerializer.Meta):
     fields = ["id", "email", "username", "password", "first_name", "last_name", 'birth_date']
+
+class UserSerializer(BaseUserSerializer):
+  class Meta(BaseUserSerializer.Meta):
+    fields = ['email', 'id', 'username', 'first_name', 'last_name']
