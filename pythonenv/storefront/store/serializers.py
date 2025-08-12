594de67319c30ba6a5c85aa17cb0d46d58e9e9af
diego orellana <diego.orellana.git@gmail.com>
2025-08-12 06:25:42 -0400

    modified serializers

--- pythonenv/storefront/store/serializers.py
+++ pythonenv/storefront/store/serializers.py
@@ -85,7 +85,7 @@ class AddCartItemSerializer(serializers.ModelSerializer):
 
 
 class CustomerSerializer(serializers.ModelSerializer):
-  user_id = serializers.IntegerField()
+  user_id = serializers.IntegerField(read_only=True)
   class Meta:
     model = Customer
     fields = ['id', 'user_id', 'phone', 'birth_date', 'membership']
