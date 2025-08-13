f212b8f4a37f1a24249cef16c180737d62d405ec
diego orellana <diego.orellana.git@gmail.com>
2025-08-13 09:44:31 -0400

    modified views

--- pythonenv/storefront/store/views.py
+++ pythonenv/storefront/store/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, UpdateModelMixin
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
 from django.db.models import Count
 from rest_framework import status
@@ -108,3 +108,16 @@ class CartItemViewSet(ModelViewSet):
 class CustomerViewSet(RetrieveModelMixin, CreateModelMixin, UpdateModelMixin, GenericViewSet):
   queryset = Customer.objects.all()
   serializer_class = CustomerSerializer
+
+  @action(detail=False, methods=['GET', 'PUT'])
+  def me(self, request):
+    (customer, created) = Customer.objects.get_or_create(user_id=request.user.id)
+    if request.method == 'GET':
+      serializer = CustomerSerializer(customer)
+      return Response(serializer.data)
+    else:
+      serializer = CustomerSerializer(customer, data=request.data)
+      serializer.is_valid(raise_exception=True)
+      serializer.save()
+      return Response(serializer.validated_data)
+
