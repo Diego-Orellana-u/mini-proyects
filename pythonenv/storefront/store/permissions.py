70187d97ef2f9e39d20b74d4b7c9bae6d756edbe
diego orellana <diego.orellana.git@gmail.com>
2025-08-22 07:51:41 -0400

    created new full django model permission with get permission bloqued

--- pythonenv/storefront/store/permissions.py
+++ pythonenv/storefront/store/permissions.py
@@ -4,3 +4,9 @@ class IsAdminOrReadOnly(permissions.BasePermission):
 
   def has_permission(self, request, view):
     return bool(request.method in permissions.SAFE_METHODS or request.user and request.user.is_staff)
+  
+
+class FullDjangoModelPermissions(permissions.DjangoModelPermissions):
+
+  def __init__(self):
+    self.perms_map['GET'] = ['%(app_label)s.view_%(model_name)s']
