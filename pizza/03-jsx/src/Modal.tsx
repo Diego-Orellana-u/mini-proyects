e096731f8f1ab5fbbaf3efd338b4d7d8598a9b6d
diego orellana <diego.orellana.git@gmail.com>
2025-09-21 09:02:33 -0300

    created new modal component

---
+++ pizza/03-jsx/src/Modal.tsx
@@ -0,0 +1,28 @@
+import { ReactElement, useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+const Modal = ({ children }) => {
+  const elRef = useRef(null);
+  if (!elRef.current) {
+    elRef.current = document.createElement("div");
+  }
+  const a = "ok" as const;
+  const b = 42 as const;
+
+  const arr = [a, b];
+
+  console.log(da);
+
+  useEffect(() => {
+    const modalRoot = document.getElementById("modal");
+    modalRoot.appendChild(elRef.current);
+
+    return () => {
+      modalRoot.removeChild(elRef.current);
+    };
+  }, []);
+
+  return createPortal(<div>{children}</div>, elRef.current);
+};
+
+export default Modal;
